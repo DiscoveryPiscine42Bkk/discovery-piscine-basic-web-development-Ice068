0f3727f2e130bd70b3697d8925e9bdff10205430
Tanatap Treerapragon <thanathep.ice@gmail.com>
2025-06-16 11:35:38 +0000

    argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-
-echo $1
-echo $2
-echo $3
+if [ $# -eq 0 ]; then
+    echo "No arguments passed."
+else
+    # แสดงผลอาร์กิวเมนต์ที่ 1 ถึง 3 ถ้ามี
+    echo "$1"
+    if [ $# -ge 2 ]; then
+        echo "$2"
+    fi
+    if [ $# -ge 3 ]; then
+        echo "$3"
+    fi
+fi
